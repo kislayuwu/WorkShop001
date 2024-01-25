1da3a5ad635ba430b068c1a2ee2aa32a11ad6dba
Kislay Shekhar <kislayshekhar2@gmail.com>
2024-01-25 11:38:08 +0530

    Array of AddressBook created to create multiple address books

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to address book");
-        AddressBook a1 = new AddressBook();
-        Service s1 = new Service();
-        Person p1 = new Person();
 
-        for(int i=0;i<2;i++) {
-            s1.setValues(p1);
-            System.out.println(s1.display(p1));
-            a1.contactList.put(p1.getFirstName(), p1);
-        }
-        for (Map.Entry<String, Person> entry : a1.contactList.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
-        }
+        //Array to create multiple Address BOOKS
+        AddressBook[] addressBookArr=new AddressBook[3];
+        for(int j=0;j< addressBookArr.length;j++) {
+            addressBookArr[j] = new AddressBook();
+            Service s1 = new Service();
+            Person p1 = new Person();
 
-        Scanner sc=new Scanner(System.in);
+            for (int i = 0; i < 2; i++) {
+                s1.setValues(p1);
+                System.out.println(s1.display(p1));
+                addressBookArr[j].contactList.put(p1.getFirstName(), p1);
+            }
+            for (Map.Entry<String, Person> entry : addressBookArr[j].contactList.entrySet()) {
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+            }
 
-        //edit using person's name
+            Scanner sc = new Scanner(System.in);
+
+            //edit using person's name
     /*  System.out.println("Enter the first name of contact to edit contact");
         String edit=sc.next();
         s1.setValues(p1);
@@ -28,21 +32,21 @@ public class Main {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }*/
 
-        //Edit using first name
-        System.out.println("Enter the first name of contact to edit contact");
-        String edit=sc.next();
-        s1.edit(edit,a1,p1);
+            //Edit using first name
+            System.out.println("Enter the first name of contact to edit contact");
+            String edit = sc.next();
+            s1.edit(edit, addressBookArr[j], p1);
 
 
-        //delete using first name
-        System.out.println("Enter the first name of contact to delete contact");
-        String delete=sc.next();
-        s1.delete(delete,a1);
+            //delete using first name
+            System.out.println("Enter the first name of contact to delete contact");
+            String delete = sc.next();
+            s1.delete(delete, addressBookArr[j]);
 
-        for (Map.Entry<String, Person> entry : a1.contactList.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
+            for (Map.Entry<String, Person> entry : addressBookArr[j].contactList.entrySet()) {
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+            }
         }
-
 
     }
 }
